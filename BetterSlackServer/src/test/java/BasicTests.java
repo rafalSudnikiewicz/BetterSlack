e23749c04689e2741e56f2cdaee9d0222a6da34a
Rafal Sudnikiewicz <r.sudnikiewicz@gmail.com>
2019-03-02 11:51:02 +0100

    add basic test

---
+++ BetterSlackServer/src/test/java/BasicTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class BasicTests {
+    @Test
+    public void basicTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
